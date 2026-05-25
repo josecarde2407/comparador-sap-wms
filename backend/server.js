857792c8b9a4e160225a21057c5d5f46c5700b2f
josecarde2407 <126813212+josecarde2407@users.noreply.github.com>
2026-05-25 17:33:19 -0500

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,22 @@ const express = require("express");
 const cors = require("cors");
 
 const compareRoutes = require("./routes/compare.routes");
+const cleanUploads = require("./utils/cleanUploads");
 
 const app = express();
 
+// CORS (mejorable para producción luego)
 app.use(cors());
 
 app.use(express.json());
 
 app.use("/api", compareRoutes);
 
-const PORT = 3001;
+// Limpieza automática cada 30 min
+setInterval(cleanUploads, 30 * 60 * 1000);
 
-const cleanUploads = require("./utils/cleanUploads");
-
-// Ejecuta cada 30 minutos
-setInterval(cleanUploads, 30* 60 * 1000);
+// IMPORTANTE: puerto dinámico para deploy
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
